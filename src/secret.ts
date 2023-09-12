1b3fbe9c0551e636a4e7eecd0ad9a00b9fac8552
Patrick McLin <pmclin@purdue.edu>
2023-09-12 14:29:35 -0400

    working?

--- src/secret.ts
+++ src/secret.ts
@@ -1,10 +1,10 @@
 // This file contains the secret functions that you need to implement.
 // In no particular order, they are:
 // - Exponent
-// - Factorial
+// - Factorial-
 // - Fibonacci
-// - Modulus
-// - Square root
+// - Modulus-
+// - Square root-
 
 // Implements the following input to output mapping:
 // 1, 2 -> 1
@@ -13,7 +13,7 @@
 // 7, 4 -> 3
 // All inputs must be >= 1
 export function secret_1(_num1: number, _num2: number): number {
-    return 0;
+    return _num1 % _num2;
 }
 
 // Implements the following input to output mapping:
@@ -23,7 +23,7 @@ export function secret_1(_num1: number, _num2: number): number {
 // 7, 4 -> 2401
 // All inputs must be >= 1
 export function secret_2(_num1: number, _num2: number): number {
-    return 0;
+    return Math.pow(_num1, _num2);
 }
 
 // Implements the following input to output mapping:
@@ -33,7 +33,7 @@ export function secret_2(_num1: number, _num2: number): number {
 // 256 -> 16
 // All inputs must be >= 0
 export function secret_3(_num1: number): number {
-    return 0;
+    return Math.sqrt(_num1);
 }
 
 // Implements the following input to output mapping:
@@ -43,7 +43,10 @@ export function secret_3(_num1: number): number {
 // 7 -> 5040
 // All inputs must be >= 0
 export function secret_4(_num1: number): number {
-    return 0;
+    for (let i = 1; i <= _num1; i++) {
+        _num1 = _num1 * i;
+    }
+    return _num1;
 }
 
 // Implements the following input to output mapping:
@@ -53,5 +56,14 @@ export function secret_4(_num1: number): number {
 // 7 -> 13
 // All inputs must be >= 1
 export function secret_5(_num1: number): number {
+    if (_num1 == 0) {
+        return 0;
+    } else if (_num1 == 1) {
+        return 1;
+    } else if (_num1 == 2) {
+        return 1;
+    } else {
+        return secret_5(_num1 - 1) + secret_5(_num1 - 2);
+    }
     return 0;
 }
